9b0f6dd22aa932907e1901007181fbdf0586da1e
Thiago Garcia <tgalves18@gmail.com>
2018-02-19 20:06:33 -0300

    Final commit

---
+++ Bow Ties/Bowtie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by Thiago Garcia on 19/02/18.
+//  Copyright © 2018 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Bowtie)
+public class Bowtie: NSManagedObject {
+
+}
